bc0794675c7dc7b17212f16eea960aa690db8500
johanedayala <johanedayala@gmail.com>
2018-11-04 17:21:19 -0600

    add some other sources

--- institutions/urls.py
+++ institutions/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from institutions import views
+from django.contrib import admin
 
 urlpatterns = [
+    url('', views.post_list, name='post_list'),
     url(r'^new$', views.new_list, name='new_list'),
     url(r'^(\d+)/$', views.view_list, name='view_list'),
     url(r'^(\d+)/add_item$', views.add_item, name='add_item'),
--- superlists/urls.py
+++ superlists/urls.py
@@ -20,6 +20,7 @@ from lists import urls as list_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url('', include('institutions.urls')),
     url(r'^$', list_views.home_page, name='home'),
     #url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
