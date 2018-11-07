9406765c29c00d2af9ee195befcb0d6c5e2d814a
johanedayala <johanedayala@gmail.com>
2018-11-06 20:33:40 -0600

    Institutions entity created.

--- lists/views.py
+++ lists/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from lists.models import Item, List
 from django.shortcuts import redirect, render
+
+from django.utils import timezone
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from institutions.models import Institutions, Inst
+
+from institutions.forms import InstForm
 # Create your views here.
 
 def home_page(request):
@@ -20,3 +28,17 @@ def add_item(request, list_id):
     list_ = List.objects.get(id=list_id)
     Item.objects.create(text=request.POST['item_text'], list=list_)
     return redirect(f'/lists/{list_.id}/')
+
+    #-------------------------VIEWS INSTITUTION-----------------------
+
+    def post_list(request):
+        posts = Inst.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
+        return render(request, 'institutions/base.html', {'posts': posts})
+
+    def post_detail(request, pk):
+        post = get_object_or_404(Inst, pk=pk)
+        return render(request, 'institutions/institution_detail.html', {'post': post})
+
+    def post_new(request):
+        form = InstForm()
+        return render(request, 'institutions/institution_edit.html', {'form': form})
